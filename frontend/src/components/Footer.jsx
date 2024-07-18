117cbeb16306f1d1dc9e01e675cea7d1661a1e5c
Rana718 <ranadolui718@gmail.com>
2024-07-18 23:54:44 +0530

    Footer.jsx

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="bg-slate-800 text-white flex flex-col justify-center items-center w-full py-6">
+      
+      <div className="text-gray-400 text-sm">
+        &copy; 2024 PassGuard. All rights reserved.
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
