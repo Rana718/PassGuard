8359003ea3cf315e805224ad8e29da1233be486f
Rana718 <ranadolui718@gmail.com>
2024-07-18 23:58:07 +0530

    env

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,7 @@
 import React from "react"
 import Navbar from "./components/Navbar"
+import Footer from "./components/Footer"
+import Manager from "./components/Manager"
 
 function App() {
   
@@ -7,6 +9,10 @@ function App() {
   return (
     <>
       <Navbar/>
+      <div className="bg-green-50 bg-[linear-gradient(to_right,#8080800a_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]">
+        <Manager />
+      </div>
+      <Footer/>
     </>
   )
 }
