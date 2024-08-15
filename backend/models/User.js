25e11c47e393dd777470e5bbb1eccfad8e5b6288
Rana718 <ranadolui718@gmail.com>
2024-08-15 22:54:17 +0530

    add Models

---
+++ backend/models/User.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    username: { type: String, required: true },
+    googleId: { type: String },
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
